8e351a7951439f3e4394d4a33a8e991a2e1017ab
mnemotiv <mantas@novatemple.com>
2017-08-28 13:47:07 +0300

    Maybe fix WP multisite tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,28 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package WP_Cache_Press
+ */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+// Determine the tests directory (from a WP dev checkout).
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+// Fallback to the default tests directory.
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/wp-rocket.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-
-activate_plugin( 'wp-rocket/wp-rocket.php' );
